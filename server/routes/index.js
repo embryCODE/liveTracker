7e06bcc569e95d03f5bf7e6545943a6920c84772
Mason Embry <mason@embrycode.com>
2017-01-12 12:52:39 -0600

    Removed concerts route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,10 @@ var router = require('express').Router();
 
 // require routes
 var users = require('./users');
-var concerts = require('./concerts');
 var auth = require('./auth');
 
 // use routes
 router.use('/api', users);
-router.use('/api', concerts);
 router.use('/auth', auth);
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@
 var router = require('express').Router();
 var userCtrl = require('../controllers').UserCtrl;
 var spotifyCtrl = require('../controllers').SpotifyCtrl;
+var jamBaseCtrl = require('../controllers').JamBaseCtrl;
 
 
 router.get('/users', userCtrl.getAllUsers);
@@ -14,5 +15,7 @@ router.get('/users/:id', userCtrl.getUser);
 router.post('/users/:id/add-zip', userCtrl.addZip);
 
 router.get('/users/:id/add-top-artists', spotifyCtrl.getUserTopArtists);
+
+router.get('/users/:id/get-local-concerts', jamBaseCtrl.getLocalConcerts);
 
 module.exports = router;
