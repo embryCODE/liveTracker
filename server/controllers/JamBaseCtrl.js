419c1a1d5673c16b9a3caad2f3da9dc327854fa4
Mason Embry <mason@embrycode.com>
2017-01-13 02:29:08 -0600

    Working on JamBase API logic.

--- server/controllers/JamBaseCtrl.js
+++ server/controllers/JamBaseCtrl.js
@@ -22,35 +22,58 @@ module.exports.getLocalConcerts = function(req, res, next) {
   User.findById(req.params.id)
     .then(function(user) {
 
-      var topArtistsTest = ['Gil Gann'];
-
-      // logic to query api, match with top artists, and build concerts array
+      // array to hold found local concerts
       var localConcerts = [];
 
+      // initial api call to get first page
       jambase.getEventListBy_zipCode_radius_startDate_endDate(user.zip, 50, today, nextYear, 0, function(err, results) {
-          if (err) {
-            res.json(err);
-          }
-          results.Events.forEach(function(event) {
-            event.Artists.forEach(function(artist) {
-              if (topArtistsTest.indexOf(artist.Name) > -1) {
-                var newEvent = {
-                  artist: artist.Name,
-                  venue: event.Venue.Name,
-                  venueURL: event.Venue.Url,
-                  date: event.Date
-                };
+        if (err) {
+          res.json(err);
+        }
 
-                localConcerts.push(newEvent);
-              }
-            });
+        // calculate necessary total pages
+        var pagesToQuery = Math.round(results.Info.TotalResults / 50);
+
+        // add found concerts from first page to localConcerts array
+        localConcerts.concat(checkConcertsForArtists(results, user.topArtists));
+
+        // loop through remaining pages, querying API for each page
+        for (var i = 1; i < pagesToQuery; i++) {
+
+          // query current page in loop
+          jambase.getEventListBy_zipCode_radius_startDate_endDate(user.zip, 50, today, nextYear, i, function(err, results) {
+            // add found concerts from current page to localConcerts array
+            localConcerts.concat(checkConcertsForArtists(results, user.topArtists));
           });
-          res.json(localConcerts);
-        });
+        }
+
+        res.json(localConcerts);
+
+      });
 
     }).catch(function(error) {
       res.json(error);
     });
+
+  // returns an array of found concerts. takes results from api and user object.
+  function checkConcertsForArtists(concerts, artists) {
+    var concertList = [];
+    concerts.Events.forEach(function(event) {
+      event.Artists.forEach(function(artist) {
+        if (artists.indexOf(artist.Name) > -1) {
+          var newEvent = {
+            artist: artist.Name,
+            venue: event.Venue.Name,
+            venueURL: event.Venue.Url,
+            date: event.Date
+          };
+
+          concertList.push(newEvent);
+        }
+      });
+    });
+    return concertList;
+  }
 };
 
 // localConcerts: [
