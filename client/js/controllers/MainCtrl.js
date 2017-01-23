1f673141d1b60bd9f3077ce33fe8bcd62e3e7c75
Mason Embry <mason@embrycode.com>
2017-01-23 10:51:02 -0600

    Zip code section complete

--- client/js/controllers/MainCtrl.js
+++ client/js/controllers/MainCtrl.js
@@ -6,7 +6,12 @@ app.controller('MainCtrl', function ($scope, $location, apiService) {
   // add current user's info to scope. redirect to /login if no current user
   apiService.getCurrentlyAuthorizedUser()
     .then(function (response) {
-      $scope.currentUser = response.data
+      apiService.addTopArtistsToUser(response.data._id)
+        .then(function (response) {
+          $scope.currentUser = response.data
+        }, function (err) {
+          console.log(err)
+        })
     }, function (err) {
       console.log(err.status + ': No authorized user found. Redirecting to /login.')
       $location.path('/login')
