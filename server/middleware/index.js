053164d7bc9b6d80f7d0a5ea034ee989f0108f87
Mason Embry <mason@embrycode.com>
2017-01-24 09:05:30 -0600

    Enabled authentication middleware.

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,13 +1,10 @@
 'use strict'
 
 module.exports.authCheck = function (req, res, next) {
-  // FAKE AUTHORIZATION BECAUSE I DON'T GET IT YET
-  return next()
-
-  // if (req.isAuthenticated()) {
-  //   console.log('Request is authenticated.')
-  //   return next()
-  // }
-  // console.log('Request is not authenticated.')
-  // res.status(401).json('You must login to Spotify.')
+  if (req.isAuthenticated()) {
+    console.log('Request is authenticated.')
+    return next()
+  }
+  console.log('Request is not authenticated.')
+  res.status(401).json('Login to Spotify and try that again.')
 }
