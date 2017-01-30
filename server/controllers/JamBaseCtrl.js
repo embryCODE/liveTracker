d8127fd3144fe85a8424f14bebd3b2aacfdccf1f
Mason Embry <mason@embrycode.com>
2017-01-30 10:00:03 -0600

    Jambase API complete. Also enabled mock jambase data.

--- server/controllers/JamBaseCtrl.js
+++ server/controllers/JamBaseCtrl.js
@@ -3,34 +3,49 @@
 var JamBase = require('node-jambase')
 var key = require('../../config/apiConfig').jambase.api_key
 var jambase = new JamBase(key)
+var mockData = require('../../mock/jambaseMock.json')
 
-// get local concerts using data.zip and data.artistName on req object
-module.exports.getLocalConcerts = function (req, res, next) {
-  // set date variables
-  var today = new Date().toISOString()
-  var nextYear = new Date()
-  nextYear.setDate(nextYear.getDate() + 365)
-  nextYear = nextYear.toISOString()
+// boolean flag for using mock data or using jambase api.
+var mock = true
 
-  var artistName = req.query.artist
+// if mock is true, just send mock data. otherwise, talk to jambase.
+if (mock) {
+  module.exports.getLocalConcerts = function returnMockData () {
+    return mockData
+  }
+} else {
+  // get local concerts using data.zip and data.artistName on req object
+  module.exports.getLocalConcerts = function (req, res, next) {
+    // set date variables
+    var today = new Date().toISOString()
+    var nextYear = new Date()
+    nextYear.setDate(nextYear.getDate() + 365)
+    nextYear = nextYear.toISOString()
 
-  jambase.getArtistBy_name(artistName, 0, function (err, response) {
-    if (err) return next(err)
+    var artistName = req.query.artist
 
-    if (response.Artists.length > 0) {
-      // save artist id that was just looked up
-      var artistId = response.Artists[0].Id
-      var zip = parseInt(req.query.zip)
+    jambase.getArtistBy_name(artistName, 0, function (err, response) {
+      if (err) return next(err)
 
-      jambase.getEventListBy_artistId_zipCode_radius_startDate_endDate(
-        artistId, zip, 50, today, nextYear, 0, function (err, response) {
-          if (err) return next(err)
-          // formats concerts from response to only the data needed
-          var concertList = formatConcerts(artistName, response)
-          res.send(concertList)
+      if (response.Artists.length > 0) {
+        // save artist id that was just looked up
+        var artistId = response.Artists[0].Id
+        var zip = parseInt(req.query.zip)
+
+        jambase.getEventListBy_artistId_zipCode_radius_startDate_endDate(
+          artistId, zip, 50, today, nextYear, 0, function (err, response) {
+            if (err) return next(err)
+            // formats concerts from response to only the data needed
+            var concertList = formatConcerts(artistName, response)
+            res.send(concertList)
+          })
+      } else {
+        res.json({
+          message: 'No artists by that name found in JamBase.'
         })
-    }
-  })
+      }
+    })
+  }
 }
 
 function formatConcerts (artistName, rawConcertList) {
