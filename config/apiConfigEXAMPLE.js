1433c705288a542ebe0c883682ffd07af3556afb
Mason Embry <mason@embrycode.com>
2017-01-30 16:07:20 -0600

    Added apiConfig.js to gitignore and created an example apiConfig file.

---
+++ config/apiConfigEXAMPLE.js
@@ -0,0 +1,13 @@
+// rename this file to apiConfig.js and enter your api details below
+
+'use strict'
+
+module.exports = {
+  jambase: {
+    api_key: 'YOUR JAMBASE API KEY HERE'
+  },
+  spotify: {
+    client_id: 'YOUR SPOTIFY API CLIENT ID HERE',
+    client_secret: 'YOUR SPOTIFY API CLIENT SECRET HERE'
+  }
+}
