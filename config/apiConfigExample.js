44325f9f076fcc29626e1777d646bb8489ae9eac
Mason Embry <mason@embrycode.com>
2017-01-30 14:40:52 -0600

    Added config files to gitignore

---
+++ config/apiConfigExample.js
@@ -0,0 +1,13 @@
+// rename this file to apiConfig.js and enter your credentials below
+
+'use strict'
+
+module.exports = {
+  jambase: {
+    api_key: 'YOUR JAMBASE API KEY'
+  },
+  spotify: {
+    client_id: 'YOUR SPOTIFY CLIENT ID',
+    client_secret: 'YOUR SPOTIFY CLIENT SECRET'
+  }
+}
---
+++ config/dbExample.js
@@ -0,0 +1,7 @@
+// rename this file to db.js and enter your database info below
+
+'use strict'
+
+module.exports = {
+  url: 'CONNECTION TO YOUR LOCAL MONGO DATABASE'
+}
