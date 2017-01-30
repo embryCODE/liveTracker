c3c95e1cebff84e2896be924396b0e999d2ebec6
Mason Embry <masonembry81@mac.com>
2017-01-30 16:01:44 -0600

    Removed personal API keys

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   jambase: {
-    api_key: 'qp42qvjj5fezhevkpbz5ynub'
+    api_key: 'YOUR JAMBASE API KEY HERE'
   },
   spotify: {
-    client_id: 'a038f7d725304123a94e71ec30f6cd85',
-    client_secret: '742465860457443abd7faccc6e3df9f3'
+    client_id: 'YOUR SPOTIFY API CLIENT ID HERE',
+    client_secret: 'YOUR SPOTIFY API CLIENT SECRET HERE'
   }
 }
